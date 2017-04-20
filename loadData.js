832491265c6adbaf177510b5a199035ac1f8640d
Tom Tornese <tornese.t@husky.neu.edu>
2017-04-19 22:54:05 -0400

    added validation to collection creation

--- loadData.js
+++ loadData.js
@@ -7,15 +7,40 @@ MongoClient.connect(config.mongodbUri, (err, db) => {
   assert.equal(null, err);
 
   db.dropCollection("abilities", (err, result) => {
-    db.createCollection("abilities", (err, result) => {
-      cmd.run('mongoimport --db pokemonParty --collection abilities --type csv --headerline --file data/abilitydata.csv');
-    });
+    db.createCollection(
+      "abilities",
+      {
+        validator: {
+          $and: [
+            { ability_id: { $type: "int" } },
+            { ability_name: { $type: "string" } },
+            { description: { $type: "string" } }
+          ]
+        }
+      },
+      (err, result) => {
+        cmd.run('mongoimport --db pokemonParty --collection abilities --type csv --headerline --file data/abilitydata.csv');
+      }
+    );
   });
 
   db.dropCollection("pokemon", (err, result) => {
-    db.createCollection("pokemon", (err, result) => {
-      cmd.run('mongoimport --db pokemonParty --collection pokemon --type csv --headerline --file data/pokemondata.csv');
-    });
+    db.createCollection(
+      "pokemon",
+      {
+        validator: {
+          $and: [
+            { id: { $type: "int" } },
+            { name: { $type: "string" } },
+            { ability: { $type: "string" } },
+            { Evolve: { $type: "string" } }
+          ]
+        }
+      },
+      (err, result) => {
+        cmd.run('mongoimport --db pokemonParty --collection pokemon --type csv --headerline --file data/pokemondata.csv');
+      }
+    );
   });
 
   db.dropCollection("parties", (err, result) => {
